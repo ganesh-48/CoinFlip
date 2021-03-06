03206f68550a8dce9fbd7c87a7ecafa83cd84205
ganesh-48 <ganesh.rananavare98@gmail.com>
2021-03-06 18:03:22 +0530

    Added Simulator Loop Through Multiple times of flipping coin and shoe number of times head and tail has won

--- coinflip.sh
+++ coinflip.sh
@@ -11,3 +11,29 @@ then
 else
 	echo "Tails"
 fi
+
+
+Head=0;
+Tail=1;
+
+
+for (( i=0; i<3; i++ ))
+do
+	randomCheck=$((RANDOM%2));
+
+	if [ $randomCheck -eq 0 ]
+	then
+		echo "Head"
+		Head=$(( $Head + 1 ))
+		count=$(( $count + 1 ))
+	elif [ $randomCheck -eq 1 ]
+	then
+		echo "Tail"
+		Tail=$(( $Tail + 1 ))
+		count=$(( $count + 1 ))
+	fi
+
+done
+
+ echo "Head=$Head"
+ echo "Tail=$Tail"
