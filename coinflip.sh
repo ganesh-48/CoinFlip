0e25d1d162a6854c333f2468cff87faf282ec491
ganesh-48 <ganesh.rananavare98@gmail.com>
2021-03-06 21:39:44 +0530

    Added to Continue Till Either of them have Won 21 times Show a win or Tie and by how much

--- coinflip.sh
+++ coinflip.sh
@@ -3,7 +3,7 @@
 echo "Welcome Flip Coin Simulation"
 
 isCoinFlip=1;
-randomCheck=$((RANDOM%2))
+randomCheck=$((RANDOM%2));
 
 if [ $isCoinFlip -eq $randomCheck ]
 then
@@ -17,7 +17,7 @@ Head=0;
 Tail=1;
 
 
-for (( i=0; i<3; i++ ))
+for (( i=0; i<100; i++ ))
 do
 	randomCheck=$((RANDOM%2));
 
@@ -26,14 +26,37 @@ do
 		echo "Head"
 		Head=$(( $Head + 1 ))
 		count=$(( $count + 1 ))
+			if [ $Head -eq 21 ]
+			then
+				break
+			fi
 	elif [ $randomCheck -eq 1 ]
 	then
 		echo "Tail"
 		Tail=$(( $Tail + 1 ))
 		count=$(( $count + 1 ))
+			if [ $Tail -eq 21 ]
+			then
+				break
+			fi
 	fi
-
 done
-
  echo "Head=$Head"
  echo "Tail=$Tail"
+
+if [ $Head -eq $Tail ]
+then
+	echo " It's Tie "
+
+elif [ $Head -gt $Tail ]
+then
+	Head=$(( $Head - $Tail ))
+	echo " Head is Win by: $Head "
+
+elif [ $Tail -gt $Head ]
+then
+	Tail=$(( $Tail - $Head ))
+	echo " Tail is Win by: $Tail "
+
+fi
+
